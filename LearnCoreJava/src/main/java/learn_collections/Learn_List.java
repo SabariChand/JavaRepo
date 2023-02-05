d84de0aef75c09c52686b855778ad1774df21476
SabariChand <csabari8795@gmail.com>
2023-02-05 17:53:31 +0530

    learn series

---
+++ LearnCoreJava/src/main/java/learn_collections/Learn_List.java
@@ -0,0 +1,18 @@
+package learn_collections;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+public class Learn_List 
+{
+@Test
+public  static void LearnArrayList()
+{
+	 ArrayList<String> a = new ArrayList<String>();
+	 a.add("Sabari");
+	 a.add("Java");
+	System.out.println(a); 
+}
+	
+}
---
+++ LearnCoreJava/src/main/java/learn_java_progs/Armstrong_number.java
@@ -0,0 +1,37 @@
+package learn_java_progs;
+
+import java.util.Scanner;
+
+public class Armstrong_number
+{
+public static void main(String[] args) {
+	
+	//An Armstrong number is one whose sum of digits raised to the power three equals the number itself. 371,
+		int n, a, i = 0, j=0;
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Enter a number");
+			n= input.nextInt();
+			a= n;
+		}
+		
+	while (a>0) {
+		i= a%10;
+		j = j+(i*i*i);
+		a=a/10;
+	
+	}
+	
+	if(n==j)
+	{
+		System.out.println("Amstrong Number");
+		
+	}
+	else
+	{
+		System.out.println("Not an armstrong number");
+	}
+}
+
+	
+	
+}
